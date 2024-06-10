606288cf9c6d96dd5173af52cf070eed29bc9785
igrios1979 <ignarios1@hotmail.com>
2024-06-10 08:55:22 -0300

    DOCKER FILE

--- msvc-usuarios/msvc-usuarios/src/main/java/org/ignaciorios/springcloud/msvc/usuarios/client/CursoClienteRest.java
+++ msvc-usuarios/msvc-usuarios/src/main/java/org/ignaciorios/springcloud/msvc/usuarios/client/CursoClienteRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-cursos",url="localhost:8082")
+@FeignClient(name="msvc-cursos",url="host.docker.internal:8082")
 public interface CursoClienteRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
